0e7d39d19e410d121a2a2f35b6c40056cbd9ab14
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-21 02:52:09 +0400

    add CatViewSet and import router

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,29 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 
-from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
 
+# from cats.views import CatList, CatDetail
+
+# создаем экземпляр класса SimpleRouter
+router = DefaultRouter()
+# зарегистрируем эндпоинты с помощью метода register()
+# парамметры метода register: URL-префикс и название вьюсета,
+#  для которого создается набор эндпоинтов
+router.register('cats', CatViewSet)
+# после регистрации  эндпоинтов надо включить новые эндпоинты
+# в список urlpatterns
+# перечень эндпоинтов доступен в router.urls
+# включим их в головной urls.py
 urlpatterns = [
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view()),
+   path('', include(router.urls))
 ]
+# только что созданный роутер сгенерирует два эндпоинта
+# теперь через эти эндпоинты доступны любые операции с моделью
+
+
+
+# urlpatterns = [
+#    path('cats/', CatList.as_view()),
+#    path('cats/<int:pk>/', CatDetail.as_view()),
+# ]
