4550889d96d669705af41d690c34294ea4acc503
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-20 17:48:22 +0400

    add changes in views.py - create view-class APICat

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,48 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Cat
 from .serializers import CatSerializer
 
-
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
+# все операции CRUD при использовании view-классов принято разделять на две группы
+# первая группа(первый класс class APICat) описывает создание нового объекта
+# и запрос всех существующих объектов
+class APICat(APIView):
+    # запрос всех существующих объектов
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+    # создание нового объекта
+    def post(self, request):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+# функция, которая добавляет в БД новую запись
+# def cat_list(request):
+#     # если метод POST, то
+#     # создаем объект сериализатора и передаем в него данные из POST-запроса
+#     if request.method == 'POST':
+#         serializer = CatSerializer(data=request.data, many=True)
+#         # если полученные данные валидны
+#         if serializer.is_valid():
+#             # то сохраняем данные в базу через метод save()
+#             serializer.save()
+#             # и возвращаем JSON со всеми данными нового объекта
+#             # и статус-код 201
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         # если же данные не валидны, возвращаем инфо об ошибке и
+#         # соответствующие данные об ошибке
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     # если же метод GET, то вью-функция должна получить из БД queryset,
+#     # в котором будут храниться запрошенные объекты модели
+#     cats = Cat.objects.all()
+#     # затем queryset нужно сериализовать. Для этого передаем
+#     # в сериализатор первым аргументом queryset
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data)
